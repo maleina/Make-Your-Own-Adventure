9673312f13d1a645336a35ff9b094f3f5358a2aa
Maleina Bidek <maleina@ucla.edu>
2021-03-31 13:40:08 -0400

    feat: Populate prompts for user to continue stories

--- MYOA/StoryNodeViewController.swift
+++ MYOA/StoryNodeViewController.swift
@@ -44,20 +44,26 @@ class StoryNodeViewController: UIViewController, UITableViewDelegate, UITableVie
     // MARK: Table Placeholder Implementation
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //TODO: Implement to push the next story node.
+        
+        // Get a StoryNodeController from the Storyboard
+        let storyNodeController = self.storyboard!.instantiateViewController(withIdentifier: "StoryNodeViewController")as! StoryNodeViewController
+        
+        // Set the story node so that we will see the start of the story
+        storyNodeController.storyNode = storyNode.storyNodeForIndex(index: (indexPath as NSIndexPath).row)
+        
+        // Push the new controller onto the stack
+        self.navigationController!.pushViewController(storyNodeController, animated: true)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // TODO: Return the number of prompts in the storyNode (The 2 is just a place holder)
-        return 2
+        return storyNode.promptCount()
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //TODO: Dequeue a cell and populate it with text from the correct prompt.
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")! 
 
-        cell.textLabel!.text = "Place holder prompt"
+        cell.textLabel!.text = storyNode.promptForIndex((indexPath as NSIndexPath).row)
         
         return cell
     }
